deeb3387093aa408bc3519d0f10b9e04d4c60277
mccalll101 <39162766+mccalll101@users.noreply.github.com>
2021-03-21 14:29:03 +0000

    mergeAttempt1
    doesn't work

---
+++ app/src/main/java/com/example/personalprofile/Read_my_reviews.java
@@ -0,0 +1,14 @@
+package com.example.personalprofile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Read_my_reviews extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_my_reviews);
+    }
+}
